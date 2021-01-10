e2f1e1f7de26521be7db28a7653e96df36f98f63
Sean Morgan <insideseanshead@gmail.com>
2021-01-09 16:43:53 -0800

    added router get request for racer card data

--- routes/garage.js
+++ routes/garage.js
@@ -4,3 +4,14 @@ const db = require('../config/database')
 const Racer = require('../models/racer')
 
 // Get Racer List
+router.get('/', (req,res) =>
+    Racer.findAll()
+        .then(racers => {
+            res.render('racers', {
+                racers
+            })
+        })
+        .catch(err => console.log(err)));
+
+
+module.exports = router;
